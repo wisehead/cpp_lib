859824ff14eb9ac092170151b46ceeca89c52a24
wisehead <alex.chenhui@gmail.com>
2018-12-17 19:13:26 +0800

    condition variables.

---
+++ cpp11/thread/condition_variable.cpp
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ *      file name: condition_variable.cpp                                               
+ *         author: Hui Chen. (c) 2018                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2018/12/17-19:09:25                              
+ *  modified time: 2018/12/17-19:09:25                              
+ *******************************************************************************/
+#include <iostream>
+using namespace std;
+// webset address: http://www.cplusplus.com/reference/condition_variable/condition_variable/%20condition_variable
+// condition_variable example
+#include <iostream>           // std::cout
+#include <thread>             // std::thread
+#include <mutex>              // std::mutex, std::unique_lock
+#include <condition_variable> // std::condition_variable
+ 
+std::mutex mtx;
+std::condition_variable cv;
+bool ready = false;
+ 
+void print_id(int id) {
+    std::unique_lock<std::mutex> lck(mtx);
+    while (!ready) cv.wait(lck);
+    // ...
+    std::cout << "thread " << id << '\n';
+}
+ 
+void go() {
+    std::unique_lock<std::mutex> lck(mtx);
+    ready = true;
+    cv.notify_all();
+}
+ 
+int main()
+{
+    std::thread threads[10];
+    // spawn 10 threads:
+    for (int i = 0; i<10; ++i)
+        threads[i] = std::thread(print_id, i);
+ 
+    std::cout << "10 threads ready to race...\n";
+    go();                       // go!
+ 
+    for (auto& th : threads) th.join();
+ 
+    return 0;
+} 
+  
