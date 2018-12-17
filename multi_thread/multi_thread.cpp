9cf1428ca3ef54addd99b40efde5e735a603d5a2
wisehead <alex.chenhui@gmail.com>
2018-12-17 14:54:42 +0800

    multi-thread.

---
+++ multi_thread/multi_thread.cpp
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ *      file name: multi_thread.cpp                                               
+ *         author: Hui Chen. (c) 2018                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2018/12/17-14:36:35                              
+ *  modified time: 2018/12/17-14:36:35                              
+ *******************************************************************************/
+#include <iostream>
+#include <thread>
+using namespace std;
+
+//示例代码1.0 http://www.cnblogs.com/feng-sc/p/5710724.html
+void threadfun1()
+{
+    std::cout << "threadfun1 - 1\r\n" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "threadfun1 - 2" << std::endl;
+}
+
+void threadfun2(int iParam, std::string sParam)
+{
+    std::cout << "threadfun2 - 1" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    std::cout << "threadfun2 - 2" << std::endl;
+}
+
+int main()
+{
+    std::thread t1(threadfun1);
+    thread t2(threadfun2, 10, "abc");
+    t1.join();
+    std::cout << "join" << std::endl;
+    t2.detach();
+    std::cout << "detach" << std::endl;
+}
