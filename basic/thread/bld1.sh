7165e46d21a92685f4d734a9f4df13f3509031fa
wisehead <alex.chenhui@gmail.com>
2019-01-04 16:06:08 +0800

    thread

---
+++ basic/thread/bld1.sh
@@ -0,0 +1,8 @@
+#############################################################
+#   File Name: bld1.sh
+#     Autohor: Hui Chen (c) 2019
+#        Mail: chenhui13@baidu.com
+# Create Time: 2019/01/04-15:56:52
+#############################################################
+#!/bin/sh 
+g++ thread1.cpp -lpthread -o a.out 
