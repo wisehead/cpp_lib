819794ee09879c369a5fd91b079b89c5c52a51a3
wisehead <alex.chenhui@gmail.com>
2019-01-04 14:01:14 +0800

    basic/cout

---
+++ basic/cout/setw.cpp
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ *      file name: setw.cpp                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/01/04-12:59:21                              
+ *  modified time: 2019/01/04-12:59:21                              
+ *******************************************************************************/
+#include <iostream>
+using namespace std;
+ 
+#include <iomanip>
+using std::setw;
+ 
+int main ()
+{
+   int n[ 10 ]; // n 是一个包含 10 个整数的数组
+ 
+   // 初始化数组元素          
+   for ( int i = 0; i < 10; i++ )
+   {
+      n[ i ] = i + 100; // 设置元素 i 为 i + 100
+   }
+   cout << "Element" << setw( 13 ) << "Value" << endl;
+ 
+   // 输出数组中每个元素的值                     
+   for ( int j = 0; j < 10; j++ )
+   {
+      cout << setw( 7 )<< j << setw( 13 ) << n[ j ] << endl;
+   }
+ 
+   return 0;
+}
