3765da08aa5298725734734cb1f8397391037ea8
wisehead <alex.chenhui@gmail.com>
2019-01-04 14:17:18 +0800

    time

---
+++ basic/time/current_time.cpp
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ *      file name: current_time.cpp                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/01/04-14:03:20                              
+ *  modified time: 2019/01/04-14:03:20                              
+ *******************************************************************************/
+#include <iostream>
+#include <ctime>
+ 
+using namespace std;
+ 
+int main( )
+{
+   // 基于当前系统的当前日期/时间
+   time_t now = time(0);
+   
+   // 把 now 转换为字符串形式
+   char* dt = ctime(&now);
+ 
+   cout << "本地日期和时间：" << dt << endl;
+ 
+   // 把 now 转换为 tm 结构
+   tm *gmtm = gmtime(&now);
+   dt = asctime(gmtm);
+   cout << "UTC 日期和时间："<< dt << endl;
+}
---
+++ basic/time/tm_format.cpp
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ *      file name: tm_format.cpp                                               
+ *         author: Hui Chen. (c) 2019                             
+ *           mail: chenhui13@baidu.com                                        
+ *   created time: 2019/01/04-14:09:37                              
+ *  modified time: 2019/01/04-14:09:37                              
+ *******************************************************************************/
+#include <iostream>
+#include <ctime>
+ 
+using namespace std;
+ 
+int main( )
+{
+   // 基于当前系统的当前日期/时间
+   time_t now = time(0);
+ 
+   cout << "1970 到目前经过秒数:" << now << endl;
+ 
+   tm *ltm = localtime(&now);
+ 
+   // 输出 tm 结构的各个组成部分
+   cout << "年: "<< 1900 + ltm->tm_year << endl;
+   cout << "月: "<< 1 + ltm->tm_mon<< endl;
+   cout << "日: "<<  ltm->tm_mday << endl;
+   cout << "时间: "<< ltm->tm_hour << ":";
+   cout << ltm->tm_min << ":";
+   cout << ltm->tm_sec << endl;
+}
