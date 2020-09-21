31ef4254791b1cdad3a2f8f11977e677d78507bd
wisehead <alex.chenhui@gmail.com>
2020-09-21 17:02:24 +0800

    return trace

---
+++ cpp11/cpp11_deepdive/04.RTII_Auto_Decltype/4-4-2.return_trace.cpp
@@ -0,0 +1,18 @@
+        #include <iostream>
+        using namespace std;
+        template<typename T1, typename T2>
+        auto Sum(const T1 & t1, const T2 & t2) -> decltype(t1 + t2){
+            return t1 + t2;
+        }
+        template <typename T1, typename T2>
+        auto Mul(const T1 & t1, const T2 & t2) -> decltype(t1 * t2){
+            return t1 * t2;
+        }
+        int main() {
+            auto a = 3;
+            auto b = 4L;
+            auto pi = 3.14;
+            auto c = Mul(Sum(a, b), pi);
+            cout << c << endl;   // 21.98
+        }
+        // 编译选项:g++ -std=c++11 4-4-2.cpp
