2aff0447dca9c07b7f96a917a07d2dff519a45f7
wisehead <alex.chenhui@gmail.com>
2020-09-22 16:27:26 +0800

    release_acquire

---
+++ cpp11/cpp11_deepdive/06.Performance_Hardware_constexpr_atomic/6-3-8.release_acquire.cpp
@@ -0,0 +1,23 @@
+        #include <thread>
+        #include <atomic>
+        #include <iostream>
+        using namespace std;
+        atomic<int> a;
+        atomic<int> b;
+        int Thread1(int) {
+            int t = 1;
+            a.store(t, memory_order_relaxed);
+            b.store(2, memory_order_release); // 本原子操作前所有的写原子操作必须完成
+        }
+        int Thread2(int) {
+            while(b.load(memory_order_acquire) != 2);   // 本原子操作必须完成才能执行之后所有 的读原子操作
+            cout << a.load(memory_order_relaxed) << endl;    // 1
+        }
+        int main() {
+            thread t1(Thread1, 0);
+            thread t2(Thread2, 0);
+            t1.join();
+            t2.join();
+            return 0;
+        }
+        // 编译选项:g++ -std=c++11 6-3-8.cpp -lpthread
