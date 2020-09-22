64ca3755c9d0c70935744fc48055f244d7c6cec6
wisehead <alex.chenhui@gmail.com>
2020-09-22 19:42:34 +0800

    for_each

---
+++ basic/for_each/4-5-2.for_each.cpp
@@ -0,0 +1,11 @@
+        #include <algorithm>
+        #include <iostream>
+        using namespace std;
+        int action1(int & e){ e *= 2; }
+        int action2(int & e){ cout << e << '\t'; }
+        int main() {
+            int arr[5] = { 1, 2, 3, 4, 5};
+            for_each(arr, arr + sizeof(arr)/sizeof(arr[0]), action1);
+            for_each(arr, arr + sizeof(arr)/sizeof(arr[0]), action2);
+        }
+        // 编译选项:g++ -std=c++11-c
