7b74f79d38f2c82c9751730deac1fbed2ed9a494
wisehead <alex.chenhui@gmail.com>
2020-09-22 14:01:27 +0800

    6-2-4.function_parameter_pack.cpp

---
+++ cpp11/cpp11_deepdive/06.Performance_Hardware_constexpr_atomic/6-2-4.function_parameter_pack.cpp
@@ -0,0 +1,26 @@
+        #include <iostream>
+        #include <stdexcept>
+        using namespace std;
+        void Printf(const char* s) {
+            while (*s) {
+                if (*s == '%' && *++s != '%')
+                    throw runtime_error("invalid format string: missing arguments");
+                cout << *s++;
+            }
+        }
+
+        template<typename T, typename... Args>
+        void Printf(const char* s, T value, Args... args) {
+            while (*s) {
+                if (*s == '%' && *++s != '%') {
+                    cout << value;
+                    return Printf(++s, args...);
+                }
+                cout << *s++;
+            }
+            throw runtime_error("extra arguments provided to Printf");
+        }
+        int main() {
+            Printf("hello %s\n", string("world"));   // hello world
+        }
+        // 编译选项:g++ -std=c++11 6-2-4.cpp
