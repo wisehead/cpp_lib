436b90e19144715050dba48bab7b05b8ff1334aa
wisehead <alex.chenhui@gmail.com>
2020-09-23 15:03:20 +0800

    unicode convert releated

---
+++ cpp11/cpp11_deepdive/08.Practice_align_unicode/8-3-3.unicode_convert.cpp
@@ -0,0 +1,11 @@
+        #include <iostream>
+        #include <cuchar>
+        using namespace std;
+        int main() {
+            char16_t utf16[] = u"\u4F60\u597D\u554A";
+            char mbr[sizeof(utf16)*2] = {0};     // 这里我们假设buffer这么大就够了
+            mbstate_t s;
+            c16rtomb(mbr, utf16, &s);
+            cout << mbr << endl;
+        }
+        // 编译选项:g++ -std=c++11-c 8-3-3.cpp
