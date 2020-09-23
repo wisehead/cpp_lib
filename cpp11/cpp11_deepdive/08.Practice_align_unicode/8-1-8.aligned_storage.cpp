e60b5ae573d52648686a2008f0a39d562375245e
wisehead <alex.chenhui@gmail.com>
2020-09-23 13:40:05 +0800

    align and aligned_storage

---
+++ cpp11/cpp11_deepdive/08.Practice_align_unicode/8-1-8.aligned_storage.cpp
@@ -0,0 +1,21 @@
+        #include <iostream>
+        #include <type_traits>
+        using namespace std;
+        // 一个对齐值为4的对象
+        struct IntAligned{
+          int a;
+          char b;
+        };
+        // 使用aligned_storage使其对齐要求更加严格
+        typedef  aligned_storage<sizeof(IntAligned),alignof(long  double)>::type
+    StrictAligned;
+        int main() {
+          StrictAligned sa;
+          IntAligned* pia = new (&sa) IntAligned;
+          cout << alignof(IntAligned) << endl;        // 4
+          cout << alignof(StrictAligned) << endl;    // 16
+          cout << alignof(*pia) << endl;               // 4
+          cout << alignof(sa) << endl;                  // 16
+          return 0;
+        }
+        // 编译选项:g++ -std=c++11 8-1-8.cpp
